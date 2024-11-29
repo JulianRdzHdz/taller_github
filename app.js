0ab02fd8b3c12c8a3fd18990d1bddf170100faef
JulianRdzHdz <a2223330187@alumnos.uat.edu.mx>
2024-11-29 13:41:16 -0600

    last las version

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const chalk = require('chalk');
+const figlet = require('figlet');
+
 console.log('Hola Mundoooo!');
+figlet('Hola Mundo!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong....');
+        console.dir(err);
+        return;
+    }
+    console.log(chalk.blue(data));
+});
